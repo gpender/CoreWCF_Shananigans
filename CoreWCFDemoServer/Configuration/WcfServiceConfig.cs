3e3dbaa9a58cb9bb62d44035ca1ae6e6bd17f544
Guy Pender <guy.s.pender@dotConnected.com>
2022-08-23 09:28:49 +0100

    Add WcfServiceConfig

---
+++ CoreWCFDemoServer/Configuration/WcfServiceConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreWCFDemoServer.Configuration
+{
+	public class WcfServiceConfig
+	{
+		/// <summary>
+		/// Base address and scheme that the service is hosted on i.e. dev =  http://localhost:8088, rockwell = http://PegasusAuditingServices
+		/// </summary>
+		public string? ServiceBaseAddress { get; set; }
+	}
+}
